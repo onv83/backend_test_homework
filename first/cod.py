cf2d206c8eb1ca56f6e6399f7daeb5b843a17d6b
Николай Опарин <demonnikolay@gmail.com>
2021-11-19 16:53:43 +0700

    # игнорить 01_Sprint # /01_Sprint

--- first/cod.py
+++
@@ -1,8 +0,0 @@
-def plus(a, b)
-    print(a- + b)
-
-plus(5, 6)
-plus(5, 7)
-#gdrgede
-
-# trxe
--- test_program.py
+++ test_program.py
@@ -8,7 +8,8 @@ files_list = ['program.py', 'readme.md']
 
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
+        assert filename in dir_files, (f'Файл `{filename}` '
+                                       f'не найден в корне репозитория')
 
     try:
         import program
@@ -18,4 +19,4 @@ def test_program():
             'Исправьте в нем ошибки:\n'
             f'{e}'
         )
-# test
+
